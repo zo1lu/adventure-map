c405e64828257f74d3ab7a47094aa85914a58a8d
Zoey Lu <luchin601@gmail.com>
2023-11-08 15:35:05 +0800

    Add fake data for testing

---
+++ src/data/fake_data.ts
@@ -0,0 +1,26 @@
+const mapInfo_fake = {
+    title: "Japan Adventure",
+    country: "Japan",
+    region_or_district: "Shiga-ken, Kyoto Prefecture",
+    location: "Japan, Kyoto",
+    travel_type: "Local Food",
+    member_type: "Friends Group",
+    images: ["/images/mapImages/japan_kyoto_01.jpg"],
+    startTime: new Date("2023-12-10T09:00"),
+    endTime: new Date("2023-12-15T18:00"),
+    duration: 105, // Duration in hours
+    description: "Embarking on an unforgettable adventure to Kyoto, Japan, with my closest friends has been a dream come true. We've immersed ourselves in the rich culture and breathtaking landscapes of Kyoto Prefecture, from the serene temples and lush bamboo forests of Arashiyama to the bustling streets of Gion. The exquisite cuisine, including traditional kaiseki meals and savory street food, has tantalized our taste buds. Our days have been filled with hiking, exploring historic sites, and engaging with the local community. As the sun sets on this remarkable journey, we carry with us memories that will last a lifetime."
+};
+
+const spotInfo_fake = {
+    title: "Chihkan Tower",
+    spot_type: ["History"],
+    location: [120.200125, 23.000856],
+    images: ["/images/spotImages/kyoto_snack.jpg"],
+    startTime: new Date("2023-11-15T10:00:00"),
+    endTime: new Date("2023-11-15T12:00:00"),
+    duration: 2, // Duration in hours
+    description: "As I entered the ancient Chihkan Tower, a sense of awe washed over me. The weathered stone walls whispered stories of centuries past. With each step, I could feel the echoes of history and the spirits of those who came before. The tranquil gardens surrounding the tower offered a serene escape from the bustling city, making it a perfect place for quiet reflection and introspection. The atmosphere here was both enchanting and surreal, creating a memory I will cherish forever."
+};
+
+export {mapInfo_fake, spotInfo_fake}
