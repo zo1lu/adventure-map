3247c073af0703a7890693b08b32e7c6d27efc74
Zoey Lu <luchin601@gmail.com>
2023-11-12 14:40:48 +0800

    Organize map and spot selecttion data

--- src/data/map.ts
+++ src/data/map.ts
@@ -1,28 +1,91 @@
 const travelTypes = [
-    "🥡Local Food",
-    "🍽️Cuisine",
-    "🛍️Shopping",
-    "🧿Culture & 🏺History",
-    "🏛️Architecture & 🖼️Artistic",
-    "🚢Cruise",
-    "🌳Nature",
-    "⛰️Mountains",
-    "🚘Road Trip",
-    "🏝️Island Hopping",
-    "🍂Rural",
-    "🏠Long Stay",
-    "🎒Backpacking",
-    "🛤️Rail",
-    "🧠Wellness"
+    {
+        name:"🥡Local Food",
+        value:"local-food",
+    },
+    {
+        name:"🍽️Cuisine",
+        value:"cuisine",
+    },
+    {
+        name:"🛍️Shopping",
+        value:"shopping",
+    },
+    {
+        name:"🧿Culture & 🏺History",
+        value:"culture-and-history",
+    },
+    {
+        name:"🏛️Architecture & 🖼️Artistic",
+        value:"architecture-and-artistic",
+    },
+    {
+        name:"🚢Cruise",
+        value:"cruise",
+    },
+    {
+        name:"🌳Nature",
+        value:"nature",
+    },
+    {
+        name:"⛰️Mountains",
+        value:"mountains",
+    },
+    {
+        name:"🚘Road Trip",
+        value:"road-trip",
+    },
+    {
+        name:"🏝️Island Hopping",
+        value:"island-hopping",
+    },
+    {
+        name:"🍂Rural",
+        value:"rural",
+    },
+    {
+        name:"🏠Long Stay",
+        value:"long-stay",
+    },
+    {
+        name:"🎒Backpacking",
+        value:"backpacking",
+    },
+    {
+        name:"🛤️Rail",
+        value:"rail",
+    },
+    {
+        name:"🧠Wellness",
+        value:"wellness",
+    },        
 ];
 
 const memberTypes = [
-    "Solo Traveler",
-    "Couples",
-    "Friends Group",
-    "Small Family (3-4 members)",
-    "Large Family (5+ members)",
-    "Solo Backpacker"
+    {
+        name:"Solo Traveler",
+        value:"solo",
+    },
+    {
+        name:"Couples",
+        value:"couples",
+    },
+    {
+        name:"Friends Group",
+        value:"friends-group",
+    },
+    {
+        name:"Small Family (3-4 members)",
+        value:"small-family",
+    },
+    {
+        name:"Large Family (5+ members)",
+        value:"large-family",
+    },
+    {
+        name:"Backpacker",
+        value:"backpacker",
+    },
 ];
 
 export {travelTypes, memberTypes}
--- src/data/spot.ts
+++ src/data/spot.ts
@@ -1,14 +1,67 @@
+// const spotTypes = [
+//     "Accommodation",
+//     "Dining",
+//     "Activity",
+//     "Shopping",
+//     "Relaxation",
+//     "History",
+//     "Adventure",
+//     "Nature",
+//     "Art and Culture",
+//     "Nightlife"
+// ];
+
 const spotTypes = [
-    "Accommodation",
-    "Dining",
-    "Activity",
-    "Shopping",
-    "Relaxation",
-    "History",
-    "Adventure",
-    "Nature",
-    "Art and Culture",
-    "Nightlife"
+    {
+        name:"🛏️Accommodation",
+        value:"accommodation",
+    },
+    {
+        name:"🍴Dining",
+        value:"dining",
+    },
+    {
+        name:"🏃Activity",
+        value:"activity",
+    },
+    {
+        name:"🛍️Shopping",
+        value:"shopping",
+    },
+    {
+        name:"🍃Relaxation",
+        value:"relaxation",
+    },
+    {
+        name:"🏺History",
+        value:"history",
+    },
+    {
+        name:"🤠Adventure",
+        value:"adventure",
+    },
+    {
+        name:"🌳Nature",
+        value:"nature",
+    },
+    {
+        name:"🖼️Art and 🧿Culture",
+        value:"art-and-culture",
+    },
+    {
+        name:"🥂Nightlife",
+        value:"nightlife",
+    },
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 ];
 
 export {spotTypes}
