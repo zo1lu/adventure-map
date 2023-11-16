f9f225ecaf219bc6aa278dc86f7c7bb945937177
Zoey Lu <luchin601@gmail.com>
2023-11-16 23:52:47 +0800

    Add login and signup page

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,9 +1,39 @@
-import React from 'react'
+'use client'
+import React, {useState, useRef, useEffect} from 'react'
+import Link from 'next/link'
+import { useRouter } from 'next/navigation';
 
 const LoginPage = () => {
+  const router = useRouter()
+  const emailRef = useRef(null)
+  const passwordRef = useRef(null)
+  const [message, setMessage] = useState({"error":true,"content":""})
+  let messageColor = message.error?"red":"rgb(85, 230, 63)"
+  async function loginHandler(e:React.FormEvent){
+    e.preventDefault();
+    let email = emailRef.current.value
+    let password = passwordRef.current.value
+    if(!email || !password){
+      setMessage(()=>{
+        return{"error":true,"content":"Please enter email and password"}
+      })
+      return
+    }
+  }
+  
   return (
-    <div>LoginPage</div>
-  )
+    <div className='w-full h-screen flex flex-col items-center'>
+        <form className="w-fit h-[400px] p-5 m-auto flex flex-col" onSubmit={loginHandler} method='POST'>
+            <h1 className="text-center mb-4">Login</h1>
+            <input className="w-48 h-10 p-3 my-1 border-solid border-2 border-sky-950 rounded-md" ref={emailRef} type="email" placeholder="email"/>
+            <input className="w-48 h-10 p-3 my-1 border-solid border-2 border-sky-950 rounded-md" ref={passwordRef} type="password" placeholder="password" autoComplete="on"/>
+            <button className="w-48 h-8 px-3 mt-4 border-solid border-2 border-sky-950" >Login</button>
+            <p className='text-sm text-red mt-3'>Not sign up yet?  
+            <Link className='underline' href="/signup"> Click here</Link></p>
+            <p className='w-48 text-sm text-red mt-3' style={{color: messageColor}}>{message.content}</p>
+        </form>
+    </div>
+)
 }
 
 export default LoginPage
--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,8 +1,43 @@
-import React from 'react'
-
+'use client'
+import React, {useState, useRef} from 'react'
+import Link from 'next/link'
+import { useRouter } from 'next/navigation'
 const SignUpPage = () => {
+  const router = useRouter()
+  const usernameRef = useRef(null)
+  const emailRef = useRef(null)
+  const passwordRef = useRef(null)
+  const [message, setMessage] = useState({"error":true,"content":""})
+  let messageColor = message.error?"red":"rgb(85, 230, 63)"
+  async function signupHandler(e:React.FormEvent){
+    e.preventDefault();
+    let email = emailRef.current.value
+    let password = passwordRef.current.value
+    let username = usernameRef.current.value
+    if(!email || !password || !username){
+      setMessage(()=>{
+        return{"error":true,"content":"Please enter email, username and password"}
+      })
+      return
+    }
+}
   return (
-    <div>SignUpPage</div>
+    <>
+        <div className='w-full h-screen flex flex-col items-center'>
+            <form className="w-fit h-[400px] p-5 m-auto flex flex-col items-center" onSubmit={signupHandler} method='POST'>
+                <h1 className="text-center mb-4">Signup</h1>
+                <input className="w-48 h-10 p-3 my-1 border-solid border-2 border-sky-950 rounded-md" ref={emailRef} type="email" placeholder="email" />
+                <input className="w-48 h-10 p-3 my-1 border-solid border-2 border-sky-950 rounded-md" ref={usernameRef} type="text" placeholder="username" />
+                <input className="w-48 h-10 p-3 my-1 border-solid border-2 border-sky-950 rounded-md" ref={passwordRef} type="password" placeholder="password" name="password" autoComplete="on" />
+                <button className="w-48 h-8 px-3 mt-4 border-solid border-2 border-sky-950" type='submit'>Sign up</button>
+                <p className='w-48 text-sm  mt-3 text-center' style={{color: messageColor}}>{message.content}</p>
+                <p className='text-sm text-red mt-3'>I already have account &rarr;
+                <Link className='underline' href="/login"> Click here</Link></p>
+                <p className='text-sm text-red mt-3'>Not interested &rarr;
+                <Link className='underline' href="/"> Back Home</Link></p>
+            </form>
+        </div>
+    </>
   )
 }
 
