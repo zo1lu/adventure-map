d4fbef83ad76586cd713dd38a70ec8baa868e1b5
Zoey Lu <luchin601@gmail.com>
2023-12-05 16:02:06 +0800

    Feat- Modify explore page map list
    -Modify go to map button target
    -Add like map function
    -Remove member limit for go to public map page

--- src/components/explore/mapItem.tsx
+++ src/components/explore/mapItem.tsx
@@ -17,7 +17,7 @@ interface MapItemProps {
         endTime:string,
         isLiked:Boolean
     },
-    session:object|null,
+    session:{user:{id:string,username:string,email:string}}|null,
     goToMap:(mapId:string)=>void
     checkIsLogin:()=>Boolean
 }
@@ -26,50 +26,74 @@ const MapItem = ({data, session, goToMap, checkIsLogin}:MapItemProps) => {
   const {id, title, country, regionOrDistrict, travelType, memberType, author, duration, mapImage, description, startTime, endTime, isLiked} = data
     
     const [liked, setLiked] = useState(isLiked)
-    const likeMap = (mapId:string) => {
+    const likeMap = () => {
         const isLogin = checkIsLogin()
-        if(isLogin){
-            //send Request base on current like state
-            //if success setIsLiked
-            setLiked(()=>!liked)
+        if(isLogin&&session!=null&&session.user!=null){
+            const userId = session.user.id
+            const mapId = id
+            const body = {
+                mapId: mapId,
+                userId: userId,
+                like:!liked
+            }
+            fetch("/api/user?type=like",{
+                method:"PATCH",
+                headers:{
+                    'Content-Type':'applicattion/json'
+                },
+                body:JSON.stringify(body)
+            })
+            .then(res=>res.json())
+            .then((data)=>{
+                if(data.success){
+                    setLiked(()=>!liked)
+                }
+            })
+            .catch((e)=>{
+                console.log(e)
+            })          
         }    
     }
     return (
         <div className='w-[calc(25%-6px)] h-[330px] cursor-pointer' >
         
-        <div className='w-full h-[150px] overflow-hidden' onDoubleClick={()=>{goToMap(id)}}>
+        <div className='w-full h-[150px] overflow-hidden' >
             <Image
+                onClick={()=>{goToMap(id)}}
                 src={mapImage?mapImage.url:"/placeholder/mapThumb.jpg"}
                 width={300}
                 height={150}
                 alt="map-img"
             />
         </div>
-        <div className='w-full flex flex-col p-2 gap-2'>
-            <p className='font-bold text-xl overflow-hidden h-8'>{title}</p>
-            <div className='w-full flex justify-between'>
-                <p>📍{country}</p>
-                <p>📍{regionOrDistrict}</p>
-            </div>
-            <div className='relative flex items-center justify-between'>
-                <p className='text-xs'>{startTime.split("T")[0]}&harr;{endTime.split("T")[0]}</p>
-                <p className='text-base font-bold'>{Math.floor(duration/24)} Days {duration % 24} Hours</p>
-            </div>
-            <div className='flex gap-1'>
-                <div className='w-fit border-[1px] rounded-md border-black px-2'>
-                    <p className='text-xs'>{travelType.name}</p>
+        <div className='w-full flex flex-col p-2 gap-2' >
+            <div className='flex flex-col gap-1' onClick={()=>{goToMap(id)}}>
+                <p className='font-bold text-xl overflow-hidden h-8'>{title}</p>
+                <div className='w-full flex justify-between'>
+                    <p>📍{country}</p>
+                    <p>📍{regionOrDistrict}</p>
                 </div>
-                <div className='w-fit border-[1px] rounded-md border-black px-2'>
-                    <p className='text-xs'>{memberType.name}</p>
+                <div className='relative flex items-center justify-between'>
+                    <p className='text-xs'>{startTime.split("T")[0]}&harr;{endTime.split("T")[0]}</p>
+                    <p className='text-base font-bold'>{Math.floor(duration/24)} Days {duration % 24} Hours</p>
                 </div>
-                
+                <div className='flex gap-1'>
+                    <div className='w-fit border-[1px] rounded-md border-black px-2'>
+                        <p className='text-xs'>{travelType.name}</p>
+                    </div>
+                    <div className='w-fit border-[1px] rounded-md border-black px-2'>
+                        <p className='text-xs'>{memberType.name}</p>
+                    </div>
+                    
+                </div>
+                <div className='h-4 overflow-hidden'>
+                    <p className='text-xs text-gray-700'>{description}</p>
+                </div>
             </div>
-            <div className='h-4 overflow-hidden'>
-                <p className='text-xs text-gray-700'>{description}</p>
-            </div>
+            
             <div className='flex justify-between'>
               <div className='text-xs text-left'>{author.username}</div>
-              <button onClick={()=>{likeMap(id)}}>
+              <button onClick={()=>{likeMap()}}>
                 
                 <Image 
                   src={liked?'/icons/star-50-fill.png':'/icons/star-50.png'}
