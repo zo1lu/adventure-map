2eb64b104833dcdf75861968e9c75028d8697e2b
Zoey Lu <luchin601@gmail.com>
2023-12-07 00:43:16 +0800

    Feat- Modify message box layout

--- src/components/message/ProcessBox.tsx
+++ src/components/message/ProcessBox.tsx
@@ -6,9 +6,16 @@ interface ProcessBoxProps {
 const ProcessBox = ({title, message}:ProcessBoxProps) => {
     return (
         <div className='w-screen h-screen bg-gray-900 bg-opacity-30 overflow-hidden absolute top-0 z-40'>
-            <div className='rounded-md w-[300px] h-[200px] flex flex-col items-center justify-center absolute top-[calc(50%-100px)] left-[calc(50%-150px)] shadow-2xl bg-white '>
+            <div className='rounded-md w-[300px] h-[200px] p-3 flex flex-col items-center justify-center absolute top-[calc(50%-100px)] left-[calc(50%-150px)] shadow-2xl bg-white '>
             <h3 className='text-2xl'>{title}</h3>
-            <p>{message}</p>
+            <div className='flex flex-col py-3'>
+                
+                <div className='w-full h-20 flex'>
+                    <svg className="animate-spin h-5 w-5 bg-main-70 m-auto" viewBox="0 0 40 40"></svg>
+                </div>
+                <p className='text-xs'>{message}</p>
+            </div>
+            
             </div>
         </div>
     )
